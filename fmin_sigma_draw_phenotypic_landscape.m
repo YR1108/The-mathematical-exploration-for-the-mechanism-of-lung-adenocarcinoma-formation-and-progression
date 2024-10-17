3cf50501c2ddcb44c5e0fbae308e65dec0868b85
YR1108 <152163650+YR1108@users.noreply.github.com>
2024-10-17 14:47:26 +0800

    Update fmin_sigma_draw_phenotypic_landscape.m

--- fmin_sigma_draw_phenotypic_landscape.m
+++ fmin_sigma_draw_phenotypic_landscape.m
@@ -1,4 +1,4 @@
-%% 求解方差，并绘制能量图谱（多稳态）
+%% Solve the variance and map the energy (multistable)
 % clear
 x1=[3.08400000000000,1.09900000000000,1.09940000000000,1.09530000000000,6.28470000000000,1.14220000000000,1.09940000000000,1.00720000000000,5.12540000000000,3.12370000000000,3.03360000000000,0.164300000000000,1.13710000000000,0];
 x2=[3.15950000000000,1.09900000000000,1.09920000000000,1.09570000000000,5.49760000000000,1.14280000000000,1.09920000000000,1.44000000000000,5.12590000000000,3.12410000000000,3.03380000000000,0.164000000000000,1.13660000000000,0.432900000000000];
@@ -15,7 +15,7 @@ d3 = [e,e,e,e,e,e,e,e,e,e/(k-b*s^n*(n*x3(10)^(n-1)/(s^n+x3(10)^n)^2)),e/(k-b*s^n
 
 Z =  -log(1+0.6123*(1/sqrt(2*pi*d1(8))*exp(-(X-x1(8)).^2/(2*d1(8)))).*(1/sqrt(2*pi*d1(5))*exp(-(Y-x1(5)).^2/(2*d1(5)))) + 0.0829*(1/sqrt(2*pi*d2(8))*exp(-(X-x2(8)).^2/(2*d2(8)))).*(1/sqrt(2*pi*d2(5))*exp(-(Y-x2(5)).^2/(2*d2(5)))) + 0.3047*(1/sqrt(2*pi*d3(8))*exp(-(X-x3(8)).^2/(2*d3(8)))).*(1/sqrt(2*pi*d3(5))*exp(-(Y-x3(5)).^2/(2*d3(5)))));
 
-%% 绘制潜势图谱
+%% plot phenotypic landscape
 figure;
 surfc(X, Y, Z);
 hold on;
@@ -27,29 +27,28 @@ scatter3(x3(8), x3(5),0, 100, 'MarkerEdgeColor',[0 .7 .7], 'MarkerFaceColor','y'
 xlabel('CHEK1');
 ylabel('P53');
 
-% %% 绘制等高线图
+ %% Draw contour
 figure;
 contourf(X, Y, Z, 20);
-% hold on;
-% scatter(x1(8), x1(5), 100, 'MarkerEdgeColor',[0 .5 .5], 'MarkerFaceColor','r');
-% hold on;
-% scatter(x2(8), x2(5), 100, 'MarkerEdgeColor',[0 .6 .6], 'MarkerFaceColor','g');
-% hold on;
-% scatter(x3(8), x3(5), 100, 'MarkerEdgeColor',[0 .7 .7], 'MarkerFaceColor','y');
-% % hold on;
-% % syms x y 
-% % p=-log(0.556*(1/sqrt(2*pi*d1(8))*exp(-(x-x1(8)).^2/(2*d1(8)))).*(1/sqrt(2*pi*d1(5))*exp(-(y-x1(5)).^2/(2*d1(5)))) + 0.097*(1/sqrt(2*pi*d2(8))*exp(-(x-x2(8)).^2/(2*d2(8)))).*(1/sqrt(2*pi*d2(5))*exp(-(y-x2(5)).^2/(2*d2(5)))) + 0.345*(1/sqrt(2*pi*d3(8))*exp(-(x-x3(8)).^2/(2*d3(8)))).*(1/sqrt(2*pi*d3(5))*exp(-(y-x3(5)).^2/(2*d3(5)))));
-% % Dx=-e*diff(p,x);
-% % Dy=-e*diff(p,y);
-% % % 选择绘制区域
-% % xRange = linspace(0, 3, 20);
-% % yRange = linspace(3, 8, 20);
-% % [X, Y] = meshgrid(xRange, yRange);
-% % 
-% % % 计算在网格点上的probability flux
-% % DX = subs(Dx, {x, y}, {X, Y});
-% % DY = subs(Dy, {x, y}, {X, Y});
-% % quiver(X, Y, double(DX), double(DY), 'r'); % 'r' 表示红色箭头
+hold on;
+scatter(x1(8), x1(5), 100, 'MarkerEdgeColor',[0 .5 .5], 'MarkerFaceColor','r');
+hold on;
+scatter(x2(8), x2(5), 100, 'MarkerEdgeColor',[0 .6 .6], 'MarkerFaceColor','g');
+hold on;
+scatter(x3(8), x3(5), 100, 'MarkerEdgeColor',[0 .7 .7], 'MarkerFaceColor','y');
+hold on;
+syms x y 
+p=-log(0.556*(1/sqrt(2*pi*d1(8))*exp(-(x-x1(8)).^2/(2*d1(8)))).*(1/sqrt(2*pi*d1(5))*exp(-(y-x1(5)).^2/(2*d1(5)))) + 0.097*(1/sqrt(2*pi*d2(8))*exp(-(x-x2(8)).^2/(2*d2(8)))).*(1/sqrt(2*pi*d2(5))*exp(-(y-x2(5)).^2/(2*d2(5)))) + 0.345*(1/sqrt(2*pi*d3(8))*exp(-(x-x3(8)).^2/(2*d3(8)))).*(1/sqrt(2*pi*d3(5))*exp(-(y-x3(5)).^2/(2*d3(5)))));
+Dx=-e*diff(p,x);
+Dy=-e*diff(p,y);
+% Select draw area
+xRange = linspace(0, 3, 20);
+yRange = linspace(3, 8, 20);
+[X, Y] = meshgrid(xRange, yRange);
+% Calculate the probability flux at the grid points
+DX = subs(Dx, {x, y}, {X, Y});
+DY = subs(Dy, {x, y}, {X, Y});
+quiver(X, Y, double(DX), double(DY), 'r');
 % 
 xlabel('CHEK1');
 xlabel('CHEK1');
